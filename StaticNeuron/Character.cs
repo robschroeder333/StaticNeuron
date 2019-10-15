28f12c0eed33837bbcd2094048c74e7d5edf1e3b
robschroeder333 <theguyonthecouch7@gmail.com>
2019-10-15 14:10:13 -0700

    feat: initialize character class

---
+++ StaticNeuron/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace StaticNeuron
+{
+    class Character
+    {
+        public Point Position { get; private set; }
+
+        public Character()
+        {
+            Position = new Point(3, 3);
+        }
+    }
+}
