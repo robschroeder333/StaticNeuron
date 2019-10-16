7a492d918eae670fad6ff6e0d75e01b07d204b9b
robschroeder333 <theguyonthecouch7@gmail.com>
2019-10-16 14:56:16 -0700

    feat: init Level class

---
+++ StaticNeuron/Level.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+
+namespace StaticNeuron
+{
+    class Level
+    {
+        List<Point> level;
+
+        public Level(int width, int height)
+        {
+            level = new List<Point>();
+            level.Add(new Point(20, 20));
+        }
+    }
+}
