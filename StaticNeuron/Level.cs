c9bfe275f37f3038f469db9e6809abdd696a3a27
robschroeder333 <theguyonthecouch7@gmail.com>
2019-10-21 09:32:13 -0700

    feat: add horizontal hallway

--- StaticNeuron/Level.cs
+++ StaticNeuron/Level.cs
@@ -9,32 +9,64 @@ namespace StaticNeuron
     class Level
     {
         public List<Point> Walls { get; set; }
+        public List<Point> Windows { get; set; }
+        int tileSize = 10;
 
-        public Level()
+        public Level(int choice = 0)
         {
             Walls = new List<Point>();
-            Walls.Add(new Point(20, 20));
+            Windows = new List<Point>();
+            CreateLevel(choice);
         }
 
-        public void CreateLevel(int levelChoice)
+        void CreateLevel(int levelChoice)
         {
             Walls.Clear();
+            int tilesHigh = (Program.height - 2) / tileSize;
+            int tilesWide = (Program.width - 2) / tileSize;
+
+
             switch (levelChoice)
             {
                 case 1:
-                    Walls.Add(new Point(20, 20));
-                    Walls.Add(new Point(21, 20));
-                    Walls.Add(new Point(22, 20));
-                    Walls.Add(new Point(22, 21));
-                    Walls.Add(new Point(22, 22));
-                    Walls.Add(new Point(5, 5));
-                    Walls.Add(new Point(40, 10));
-                    Walls.Add(new Point(40, 11));
-                    Walls.Add(new Point(40, 13));
-                    Walls.Add(new Point(40, 14));
+                    
                     break;
-                default:
+                default: //testing
+                    Hall_H(new Point(1, 1));
                     break;
+            }
+        }
+
+        void Hall_H(Point origin)
+        {
+            for (int y = 0; y < tileSize; y++)
+            {
+                for (int x = 0; x < tileSize; x++)
+                {
+                    if (y == 0 || y == 9)
+                    {
+                        if (x >= 3 && x <= 5)
+                            Windows.Add(new Point(origin.X + x, origin.Y + y));
+                        else    
+                            Walls.Add(new Point(origin.X + x, origin.Y + y));
+                    }
+                    if (y == 1 || y == 8)
+                    {
+                        if (x < 3 || x > 5)
+                            Walls.Add(new Point(origin.X + x, origin.Y + y));
+                    }
+                    if (y == 2 || y == 7)
+                    {
+                        if (x > 0 && x < 3 || x > 5 && x < 9)
+                            Walls.Add(new Point(origin.X + x, origin.Y + y));
+                    }
+                    if (y == 3 || y == 6)
+                    {
+                        if (x > 1 && x < 4 || x > 4 && x < 7)
+                            Walls.Add(new Point(origin.X + x, origin.Y + y));
+                        
+                    }
+                }
             }
         }
     }
