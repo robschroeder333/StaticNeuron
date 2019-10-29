5a03963d4a2bf4dfa99914a7a74e84a87068b9d0
robschroeder333 <theguyonthecouch7@gmail.com>
2019-10-29 15:59:06 -0700

    feat: begin enemy refactor

---
+++ StaticNeuron/Enemy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace StaticNeuron
+{
+    class Enemy
+    {
+        public Point Position { get; private set; }
+        bool isVert;
+        bool isForward;
+        public Enemy(int x, int y, bool vert, bool forward = true)
+        {
+            Position = new Point(x, y);
+            isVert = vert;
+            isForward = forward;
+        }
+        void Move()
+        {
+            if (isVert)
+            {
+                if (isForward)
+                {
+                    if (Position.Y + 1 < Program.height - 1
+                        && (Game.screen[Position.X, Position.Y + 1] != Pieces.Wall
+                            || Game.screen[Position.X, Position.Y + 1] != Pieces.Window))
+                    { 
+                        Position = new Point(Position.X, Position.Y + 1);
+                    }
+                    else
+                        isForward = !isForward;
+                    
+                }
+                else
+                {
+                    if (Position.Y - 1 > 1
+                        && (Game.screen[Position.X, Position.Y + 1] != Pieces.Wall
+                            || Game.screen[Position.X, Position.Y + 1] != Pieces.Window))
+                    {
+                        Position = new Point(Position.X, Position.Y + 1);
+                    }
+                    else
+                        isForward = !isForward;
+                }
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
