e4a1136d6ec2c9ec427a4e0685b7688d3c53271c
robschroeder333 <theguyonthecouch7@gmail.com>
2019-10-30 15:43:52 -0700

    feat: add enemies to level 1 and 2

--- StaticNeuron/Enemy.cs
+++ StaticNeuron/Enemy.cs
@@ -8,69 +8,80 @@ namespace StaticNeuron
     class Enemy
     {
         public Point Position { get; private set; }
+        int counter;
+        int stepDelay;
         bool isVert;
         bool isForward;
-        public Enemy(int x, int y, bool vert, bool forward = true)
+        public Enemy(int x, int y, bool vert, int delay = 0, bool forward = true)
         {
             Position = new Point(x, y);
             isVert = vert;
             isForward = forward;
+            stepDelay = delay;
+            counter = delay;
         }
-        void Move()
+        public void Move()
         {
-            if (isVert)
+            if (counter == 0)
             {
-                if (isForward)
+                counter = stepDelay;
+
+                if (isVert)
                 {
-                    if (Position.Y + 1 < Program.height - 1
-                        && (Game.screen[Position.X, Position.Y + 1] != Pieces.Wall
-                            || Game.screen[Position.X, Position.Y + 1] != Pieces.Window))
-                    { 
-                        Position = new Point(Position.X, Position.Y + 1);
+                    if (isForward)
+                    {
+                        if (Position.Y + 1 < Program.height - 1
+                            && (Game.screen[Position.X, Position.Y + 1] != Pieces.Wall
+                                && Game.screen[Position.X, Position.Y + 1] != Pieces.Window))
+                        {
+                            Position = new Point(Position.X, Position.Y + 1);
+                        }
+                        else
+                            isForward = !isForward;
+
                     }
                     else
-                        isForward = !isForward;
-                    
+                    {
+                        if (Position.Y - 1 > 1
+                            && (Game.screen[Position.X, Position.Y - 1] != Pieces.Wall
+                                && Game.screen[Position.X, Position.Y - 1] != Pieces.Window))
+                        {
+                            Position = new Point(Position.X, Position.Y - 1);
+                        }
+                        else
+                            isForward = !isForward;
+                    }
                 }
                 else
                 {
-                    if (Position.Y - 1 > 1
-                        && (Game.screen[Position.X, Position.Y - 1] != Pieces.Wall
-                            || Game.screen[Position.X, Position.Y - 1] != Pieces.Window))
+                    if (isForward)
                     {
-                        Position = new Point(Position.X, Position.Y - 1);
+                        if (Position.X + 1 < Program.width - 1
+                            && (Game.screen[Position.X + 1, Position.Y] != Pieces.Wall
+                                && Game.screen[Position.X + 1, Position.Y] != Pieces.Window))
+                        {
+                            Position = new Point(Position.X + 1, Position.Y);
+                        }
+                        else
+                            isForward = !isForward;
+
                     }
                     else
-                        isForward = !isForward;
+                    {
+                        if (Position.X - 1 > 1
+                            && (Game.screen[Position.X - 1, Position.Y] != Pieces.Wall
+                                && Game.screen[Position.X - 1, Position.Y] != Pieces.Window))
+                        {
+                            Position = new Point(Position.X - 1, Position.Y);
+                        }
+                        else
+                            isForward = !isForward;
+
+                    }
                 }
             }
             else
-            {
-                if (isForward)
-                {
-                    if (Position.X + 1 < Program.height - 1
-                        && (Game.screen[Position.X + 1, Position.Y] != Pieces.Wall
-                            || Game.screen[Position.X + 1, Position.Y] != Pieces.Window))
-                    {
-                        Position = new Point(Position.X + 1, Position.Y);
-                    }
-                    else
-                        isForward = !isForward;
-
-                }
-                else
-                {
-                    if (Position.X - 1 > 1
-                        && (Game.screen[Position.X - 1, Position.Y] != Pieces.Wall
-                            || Game.screen[Position.X - 1, Position.Y] != Pieces.Window))
-                    {
-                        Position = new Point(Position.X - 1, Position.Y);
-                    }
-                    else
-                        isForward = !isForward;
-
-                }
-            }
+                counter--;
         }
     }
 }
